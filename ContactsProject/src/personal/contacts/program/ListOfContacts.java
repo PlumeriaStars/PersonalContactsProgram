0922e7d132bafb539b14e66f8454f36fb6b850a0
Tatiana Olalde <tatiana.olalde@gmail.com>
2024-08-15 02:15:07 -0500

    [Change] Added view functions to ListOfContacts class.
    ListOfContacts class now has functions that iterate over the contact
    ArrayList and display information. These include the option to display
    all contact's first and last name, and vice versa, as well as print
    every contact object's information. None of these functions sort the
    contact ArrayList before printing.

--- ContactsProject/src/personal/contacts/program/ListOfContacts.java
+++ ContactsProject/src/personal/contacts/program/ListOfContacts.java
@@ -2,6 +2,9 @@ package personal.contacts.program;
 
 import java.util.ArrayList;
 
+/* Contains the ArrayList of Contact objects the program will be working with
+ * Various methods related to changing or displaying from the ArrayList are here
+ */
 public class ListOfContacts
 {
 	private static ArrayList<Contact> contactsList = new ArrayList<Contact>();
@@ -14,6 +17,30 @@ public class ListOfContacts
 	public static ArrayList<Contact> getContactsList()
 	{
 		return contactsList;
+	}
+	
+	public static void viewFirstLastNames()
+	{
+		for(Contact c: contactsList)
+		{
+			System.out.println(c.getFirstName() + " " + c.getLastName());
+		}
+	}
+	
+	public static void viewLastFirstNames()
+	{
+		for(Contact c: contactsList)
+		{
+			System.out.println(c.getLastName() + " " + c.getFirstName());
+		}
+	}
+	
+	public static void viewAllContactInformation()
+	{
+		for(Contact c: contactsList)
+		{
+			System.out.println(c);
+		}
 	}
 
 }
